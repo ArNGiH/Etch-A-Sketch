fe474da6d330d39779feaa9bafa5754be5c27cc5
ArNGiH <aryannayar47@gmail.com>
2023-09-29 12:32:00 +0530

    Added Skeching Functionality to the board

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
+let color="black";
+
+
 document.addEventListener("DOMContentLoaded",function(){
     
     createBoard(16); 
+    let btn_popup=document.querySelector("#popup");
+    btn_popup.addEventListener('click',function(){
+        let size=getSize() ;
+        createBoard(size);
+
+    })
     console.log("Hi!")
 });
 
@@ -13,8 +22,40 @@ function createBoard(size){
     for(let i=0;i<numDivs;i++)
     {
         let div=document.createElement("div")
-        div.style.backgroundColor="green";
+        div.addEventListener('mouseover',colorDiv);
         board.insertAdjacentElement("beforeend",div);
         
+    }
+}
+
+function getSize(){
+    let input=prompt("What will be the size of the board?");
+    let message=document.querySelector("#message");
+    if(input==""){
+        message.innerHTML="Please Provide a Number";
+    }
+    else if(input <0 || input >100){
+        message.innerHTML="Provide a number 1 and 100";
+    }
+    else{
+        message.innerHTML="Now you can play";
+        return input;
+    }
+}
+
+
+
+function setColor(colourChoice){
+     color=colourChoice;
+    
+}
+
+function colorDiv(){
+    if(color=="random")
+    {
+        this.style.backgroundColor=`hsl(${Math.random()*360},100%,50%)`;
+    }
+    else{
+        this.style.backgroundColor='black';
     }
 }
